64a094069dfe1584c03015a2c11dde2f0b0337bf
Paul Kalupnieks <paul@bigbangtechnology.com>
2012-04-17 11:41:24 -0400

    refactored auth test

--- test/authentication.js
+++ test/authentication.js
@@ -1,19 +1,17 @@
-require ('should');
 var auth = require('../lib/authenticate.js');
 
 describe('authentication', function() {
   describe('#verify', function() {
-    it('logs the user in with good credentials', function() {
+
+    it('logs in the user with good credentials', function() {
       var params  = { email: 'test@example.com', password: 'test' };
-      var subject = auth.verify(params);
-
-      subject.should.equal(true);
+      auth.verify(params).should.equal(true);
     });
-    it('does not log in the user with missing credentials', function() {
-      var params = {};
-      var subject = auth.verify(params);
 
-      subject.should.equal(false);
+    it('does not log in the user with incorrect credentials', function() {
+      var params = {email: 'bad', password: 'bad'};
+      auth.verify(params).should.equal(false);
     });
+
   });
 });
