db38d293daa213ca22d08bef709fb8866d6db626
Paul Kalupnieks <paul@bigbangtechnology.com>
2012-04-25 14:37:45 -0400

    whitespace

--- test/url.js
+++ test/url.js
@@ -1,45 +1,46 @@
-var url    = require('../lib/url.js'),
-    should = require('should'),
-    redis  = require('redis'),
-    client = redis.createClient();
+var should  = require('should'),
+    redis   = require('redis'),
+    client  = redis.createClient(),
+    Url     = require('../lib/url.js');
 
 describe('url', function() {
+  var uri = 'http://example.com',
+  desc    = 'this is a description';
+
+
+  after(function(done) {
+    client.keys('*', function(err, replies) {
+      replies.forEach(function(reply, i) {
+        client.del(reply);
+      });
+    });
+    done();
+  });
 
   describe('#new', function() {
-    var uri = 'http://example.com',
-    desc    = 'this is a description',
-    my_url  = {};
-
-    beforeEach(function(done) {
-      client.keys('*', function(err, replies) {
-        replies.forEach(function(reply, i) {
-          client.del(reply);
-        });
-      });
-      my_url  = new url(uri, desc);
-      done();
-    });
 
     it('sets a uri', function(done) {
+      var my_url = new Url(uri, desc);
       my_url.uri(function(err, subject) {
         should.not.exist(err);
         subject.should.equal(uri);
-        return done();
+        done();
       });
     });
 
     it('sets a description', function(done) {
+      var my_url = new Url(uri, desc);
       my_url.description(function(err, subject) {
         should.not.exist(err);
         subject.should.equal(desc);
-        return done();
+        done();
       });
     });
 
   });
 
   //describe('#delete', function() {
-    //it('removes a link');
+  //it('removes a link');
   //});
 
 });
